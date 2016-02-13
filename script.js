8ea15460ef67685a5dd42143bdbe45b8aea2ac9d
BeroMaze <Berning.corey@gmail.com>
2016-02-13 12:28:36 -0800

    search rolling text

--- script.js
+++ script.js
@@ -4,3 +4,23 @@
 $height = $(window).height();
 console.log($height);
 $('#outerBox').css('height', ($height * 0.77));
+
+var txt = "Queen Anne, Sushi, Cheap, GET ME DRUNK";
+var timeOut;
+var txtLen = txt.length;
+counter =0;
+var char = 0;
+$('#searchBox').attr('placeholder', '|');
+(function typeIt() {
+    var humanize = Math.round(Math.random() * (200 - 30)) + 30;
+    timeOut = setTimeout(function () {
+        char++;
+        var type = txt.substring(0, char);
+        $('#searchBox').attr('placeholder', type + '|');
+        typeIt();
+        if (char == txtLen) {
+            $('#searchBox').attr('placeholder', $('#searchBox').attr('placeholder').slice(0, -1)); // remove the '|'
+            clearTimeout(timeOut);
+        }
+    }, humanize);
+}());
