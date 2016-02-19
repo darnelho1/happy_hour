98dcc67454acee7ad297070b04eb84dc54561759
Darnell Holder <darnelho1@gmail.com>
2016-02-18 16:44:01 -0800

    add iframe api to handle background video player

--- vendors/scripts/iframeAPI.js
+++ vendors/scripts/iframeAPI.js
@@ -9,32 +9,33 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'M7lc1UVf-VE',
+  player = new YT.Player('iframeAPIplayer', {
+    videoId: 'e_2ru111Ce4',
+    playerVars:{'controls':0,'showinfo':0,'loop':1,'playlist':'e_2ru111Ce4'},
     events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
+      'onReady': onPlayerReady    }
   });
 }
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.playVideo();
+  player.mute();
+  $('iframe').addClass('fullscreen-bg__video');
 }
+
+
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
+// var done = false;
+// function onPlayerStateChange(event) {
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     setTimeout(stopVideo, 6000);
+//     done = true;
+//   }
+// }
+// function stopVideo() {
+//   player.stopVideo();
+// }
