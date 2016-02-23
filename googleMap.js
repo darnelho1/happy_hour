2e5f168e7106e77409929e5d38105c1845374a14
BeroMaze <Berning.corey@gmail.com>
2016-02-23 12:44:37 -0800

    add animation to map

--- googleMap.js
+++ googleMap.js
@@ -65,16 +65,28 @@ function mapFunction() {
   $('.resultsInfo').on('click', function(event) {
     locationId = $(this).find('.resultAddress').text();
     mapSize();
-    $('#resultsOuterBox').addClass('flipOutX animated').removeClass('flipInX').hide();
-    $('#mapView').addClass('flipInX animated').removeClass('flipOutX').show();
-    $('#searchBox').addClass('flipOutX animated').removeClass('flipInX').hide();
-    $('#backButton').addClass('flipInX animated').removeClass('flipOutX').show();
+    $('#resultsOuterBox').addClass('slideOutDown animated');
+    $('#searchBox').addClass('flipOutX animated').removeClass('flipInX');
+    setTimeout(function(){
+      $('#resultsOuterBox').hide();
+      $('#searchBox').hide();
+      $('#mapView').addClass('slideInDown animated').removeClass('slideOutUp').show();
+      $('#backButton').addClass('flipInX animated').removeClass('flipOutX').show();
+    }, 470);
+
+
+
     initMap();
     $('#backButton').on('click', function(event) {
-      $('#resultsOuterBox').addClass('flipInX').removeClass('flipOutX').show();
-      $('#mapView').addClass('flipOutX').removeClass('flipInX').hide();
-      $('#searchBox').addClass('flipInX').removeClass('flipOutX').show();
-      $('#backButton').addClass('flipOutX').removeClass('flipInX').hide();
+      $('#mapView').addClass('slideOutUp').removeClass('slideInDown');
+      $('#backButton').addClass('flipOutX').removeClass('flipInX');
+      setTimeout(function(){
+        $('#mapView').hide();
+        $('#backButton').hide();
+        $('#searchBox').addClass('flipInX').removeClass('flipOutX').show();
+        $('#resultsOuterBox').removeClass('slideOutDown').show();
+      }, 470);
+
     });
   });
 }
